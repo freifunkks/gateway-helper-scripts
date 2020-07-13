4d5db3d00f2063175972be7ee2e9716078796c49
Daniel Rippen <rippendaniel@gmail.com>
2020-07-13 15:02:39 +0200

    cleanup

--- vxlan-wg/vxlan-connect.sh
+++ vxlan-wg/vxlan-connect.sh
@@ -19,29 +19,21 @@ vxlanid="1${vxhost1}${vxhost2}"
 }
 
 function connect {
-echo "connecting t-gw-$REALHOSTNAMENUMBER to t-gw-$gw via $vxlanid"
-ip -6 link add vx${vxlanid} type vxlan id $vxlanid dstport 4789 local fcff:fdfd:fdfd:fdfd:${REALHOSTNAMENUMBER}::${REALHOSTNAMENUMBER} remote fcff:fdfd:fdfd:fdfd:${gw}::${gw} dev wgffksgws0 ttl 2
-ip link set address 56:ae:94:${gw}:75:${REALHOSTNAMENUMBER} dev vx${vxlanid}
-ip link set up dev vx${vxlanid}
-batctl if add vx${vxlanid}
+	echo "connecting gw-$REALHOSTNAMENUMBER to gw-$gw via $vxlanid"
+	ip -6 link add vx${vxlanid} type vxlan id $vxlanid dstport 4789 local fcff:fdfd:fdfd:fdfd:${REALHOSTNAMENUMBER}::${REALHOSTNAMENUMBER} remote fcff:fdfd:fdfd:fdfd:${gw}::${gw} dev wgffksgws0 ttl 2
+	ip link set address 56:ae:94:${gw}:75:${REALHOSTNAMENUMBER} dev vx${vxlanid}
+	ip link set up dev vx${vxlanid}
+	batctl if add vx${vxlanid}
 }
 
 for gw in $GWS; do
 	HOSTNAME="gw-$gw.ffks.de"
 	REALHOSTNAME="$(hostname)"
 	REALHOSTNAMENUMBER="$(echo $REALHOSTNAME|rev|cut -d'-' -f1|rev|cut -d'.' -f1)"
-	#echo "My local Hostname should be: $REALHOSTNAME; Connecting to: $HOSTNAME"
-	#echo "Local: $REALHOSTNAMENUMBER Remote: $gw"
 
 	getvxlanid
-	#echo "VXLAN ID: 1${vxhost1}${vxhost2}"
-	#echo "$vxlanid : vx${vxlanid}"
 
 	if [ "$HOSTNAME" != "$REALHOSTNAME" ]; then
-		#echo "CONNECTING $HOSTNAME"
 		connect
-#	else
-#		true
-#		#echo "NOT CONNECTING $HOSTNAME"
 	fi
 done
