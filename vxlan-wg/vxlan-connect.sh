6a7ebc579d7adc5d30d36060cb56f8701775c73f
Daniel Rippen <rippendaniel@gmail.com>
2020-07-13 15:13:10 +0200

    Update MAC-Address

--- vxlan-wg/vxlan-connect.sh
+++ vxlan-wg/vxlan-connect.sh
@@ -21,7 +21,7 @@ vxlanid="1${vxhost1}${vxhost2}"
 function connect {
 	echo "connecting gw-$REALHOSTNAMENUMBER to gw-$gw via $vxlanid"
 	ip -6 link add vx${vxlanid} type vxlan id $vxlanid dstport 4789 local fcff:fdfd:fdfd:fdfd:${REALHOSTNAMENUMBER}::${REALHOSTNAMENUMBER} remote fcff:fdfd:fdfd:fdfd:${gw}::${gw} dev wgffksgws0 ttl 2
-	ip link set address 56:ae:94:${gw}:75:${REALHOSTNAMENUMBER} dev vx${vxlanid}
+	ip link set address ba:ca:ba:${gw}:75:${REALHOSTNAMENUMBER} dev vx${vxlanid}
 	ip link set up dev vx${vxlanid}
 	batctl if add vx${vxlanid}
 }
